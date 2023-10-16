ded3cc568f39c469f57191a11eeb55af1f40add9
OtavioBraga99 <otaviobraga1325@gmail.com>
2023-10-16 09:23:09 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/otaviobraga/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/otaviobraga/cursomc/resources/CategoriaResources.java
@@ -1,11 +1,11 @@
 package com.otaviobraga.cursomc.resources;
 
 import java.net.URI;
-import java.text.Collator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -37,9 +38,7 @@ public class CategoriaResources {
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Object> insert(@RequestBody CategoriaDTO dto) {
 		CategoriaDTO savedCategoria = service.insert(dto);
-		URI uri = ServletUriComponentsBuilder
-				.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(savedCategoria.getId())
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedCategoria.getId())
 				.toUri();
 		return ResponseEntity.created(uri).build();
 	}
@@ -56,12 +55,23 @@ public class CategoriaResources {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<CategoriaDTO>> findAll(){
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
-		
 	}
+	
+	@RequestMapping(value="/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page", defaultValue = "0" ) Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue = "24" )Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue = "nome" ) String orderBy, 
+			@RequestParam(value="direction", defaultValue = "ASC" )String direction) {
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok().body(listDto);
+	}
+	
 }
--- src/main/java/com/otaviobraga/cursomc/services/CategoriaService.java
+++ src/main/java/com/otaviobraga/cursomc/services/CategoriaService.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Service;
 
 import com.otaviobraga.cursomc.domain.Categoria;
@@ -42,6 +46,7 @@ public class CategoriaService {
 		entity = repo.save(entity);
 		return mapper.toDto(entity);
 	}
+
 	@Transactional
 	public void delete(Integer id) {
 		findAll(id);
@@ -51,7 +56,13 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
 		}
 	}
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of (page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
