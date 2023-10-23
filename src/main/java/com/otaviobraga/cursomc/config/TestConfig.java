47767e6bd01267296ad2a98784b16a860d03571b
OtavioBraga99 <otaviobraga1325@gmail.com>
2023-10-23 08:10:41 -0300

    criando profile de teste

---
+++ src/main/java/com/otaviobraga/cursomc/config/TestConfig.java
@@ -0,0 +1,26 @@
+package com.otaviobraga.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.otaviobraga.cursomc.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instatiateDataBase();
+		return true;
+	}
+	
+	
+}
